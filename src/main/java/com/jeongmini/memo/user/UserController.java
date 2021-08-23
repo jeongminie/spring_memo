9a2eef4b137a63f48a2edd56fa25656eb67a07f5
LAPTOP-9ER1HVT0\opooi <jeongminiee@naver.com>
2021-08-23 09:29:34 +0900

    [윤정민]  - 메모 프로젝트 로그인 까지

---
+++ src/main/java/com/jeongmini/memo/user/UserController.java
@@ -0,0 +1,32 @@
+package com.jeongmini.memo.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/signIn_view")
+	public String signInView() {
+		return "user/signIn";
+	}
+	
+	@GetMapping("/signUp_view")
+	public String signUpView() {
+		return "user/signUp";
+	}
+	
+	@GetMapping("/sign_out")
+	public String signOut (HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		session.removeAttribute("userLoginId");
+		session.removeAttribute("userName");
+		
+		return "redirect:/user/signIn_view";
+	}
+}
